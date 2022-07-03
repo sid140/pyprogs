27ae04a68f61fa657033f2325461473b5b85cd77
Siddex <920976+sid140@users.noreply.github.com>
2022-07-03 22:14:46 +0530

    Update Trickster.py

--- Trickster.py
+++ Trickster.py
@@ -1,7 +1,4 @@
 from random import choice
-import os
-from tkinter import *
-from tkinter.ttk import *
 
 print("Trickster".upper())
 print("=" * 20)
@@ -18,16 +15,20 @@ b = input("600-450=100-30")
 
 if trick == "20":
     winner.append(name)
+    print(winner)
     pass
     print("you won, {}!".format(name))
 else:
     print("{},You lost".format(name))
     loser.append(name)
+    print(loser)
     print("Better luck next time")
     if a == input("HINT"):
         print(b)
     else:
         pass
+    elif:
+         if a != input("HINT"):
 
         input("Enter the magic number: ").strip().lower()
         print("Thank you for playing")
@@ -41,8 +42,3 @@ while trick is not True:
     input("Enter the magic number: ").strip().lower()
     print("{},You lost again".format(name))
 n = input("Do you want to play again(y/n)?: ").strip().lower()
-if input("Show winner list"):
-    print(winner)
-else:
-    input("Show loser list")
-    print(loser)
