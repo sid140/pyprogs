4ce00c4abddaa56fdea9d5d74e552776a7a8a71b
Siddex <920976+sid140@users.noreply.github.com>
2022-07-03 15:47:32 +0530

    Add files via upload
    In this game you need to find the magical number

---
+++ Trickster.py
@@ -0,0 +1,49 @@
+#This is my first Python game
+print("Welcome to Trickster").strip().title()
+print("Game developed by Sid).strip().title()
+print("=" *20)
+from random import choice
+winner = []
+loser = []
+loser != winner
+print("Trickster".upper())
+print("=" * 20)
+name = input("Enter your name?:").strip().title()
+trick = input("Enter the magic number: ").strip().lower()
+n = "Want to try again?"
+if trick == "20":
+    print("you won, {}!".format(name))
+else:
+    print("You lost!")
+    loser.append(name)
+
+    print("{},You are now in loser's list".format(name))
+    input("Do you want to exit(y/n)?:").strip().lower()
+
+if input(n) is not True:
+    print()
+else:
+    print("")
+    exit(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
